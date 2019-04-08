d3aa1686fd22f50bdeabdcb4e72f65a6ae0498e9
Alejandro Pimentel <apimentel97@yahoo.com>
2019-04-07 22:20:24 -0700

    Initial Commit

---
+++ The Gram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Gram
+//
+//  Created by Alejandro pimentel on 4/7/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
