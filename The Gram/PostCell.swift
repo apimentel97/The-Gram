bfa05e583bdd633eef919b4a1313b50ae8608103
Alejandro Pimentel <apimentel97@yahoo.com>
2019-04-09 19:21:58 -0700

    Done

---
+++ The Gram/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  The Gram
+//
+//  Created by Alejandro pimentel on 4/8/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
