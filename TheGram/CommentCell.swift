e147a6bb7c1f94ec5666f8ca45205c54f3f72730
Alejandro Pimentel <apimentel97@yahoo.com>
2019-04-14 22:12:51 -0700

    Done

---
+++ TheGram/CommentCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommentCell.swift
+//  TheGram
+//
+//  Created by Alejandro pimentel on 4/13/19.
+//  Copyright © 2019 Alejandro pimentel. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
